c06f75ff7df1be3be8a660203d9c8a77d726b47c
jyoo84 <juhwanyoo@gmail.com>
2018-07-11 23:58:11 -0700

    changed argument for rgb_to_color from windowsurface to surface

--- py/common/color.py
+++ py/common/color.py
@@ -1,6 +1,6 @@
 from sdl2 import *
 
 
-def rgb_to_color_value(windowsurface, r_int, g_int, b_int):
-    return SDL_MapRGB(windowsurface.contents.format, r_int, g_int, b_int)
+def rgb_to_color_value(surface, r_int, g_int, b_int):
+    return SDL_MapRGB(surface.contents.format, r_int, g_int, b_int)
 
