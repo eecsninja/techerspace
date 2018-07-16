ae008ceeb71529f35ecc0c064a85dd64c35d806a
Simon Que <sque@chromium.org>
2018-07-15 17:52:32 -0700

    Add grid rendering

--- py_sq/tetris/grid.py
+++ py_sq/tetris/grid.py
@@ -1,3 +1,5 @@
+from sdl2 import *
+
 class Grid:
   def __init__(self, width, height):
     self.width = width
@@ -12,3 +14,24 @@ class Grid:
   def GetValue(self, x, y):
     # TODO: Check bounds.
     return self.array[x + self.width * y]
+
+
+class GridRenderer:
+  def __init__(self, block_width, block_height, palette):
+    self.block_width = block_width  # integer
+    self.block_height = block_height  # integer
+    self.palette = palette  # integer array
+
+
+  def DrawToSurface(self, grid, surface):
+    # Draws to (0, 0) of surface. Does not check whether surface is big enough.
+    # grid: Grid object
+    # surface: SDL surface
+    rect = SDL_Rect()
+    rect.w = self.block_width
+    rect.h = self.block_height
+    for y in xrange(grid.height):
+      rect.y = y * self.block_height
+      for x in xrange(grid.width):
+        rect.x = x * self.block_width
+        SDL_FillRect(surface, rect, self.palette[grid.GetValue(x, y)])
--- py_sq/tetris/main.py
+++ py_sq/tetris/main.py
@@ -14,9 +14,9 @@ SCREEN_HEIGHT = 480
 # Game defs.
 GRID_WIDTH = 10
 GRID_HEIGHT = 20
-
-# Game data.
-game_grid = grid.Grid(GRID_WIDTH, GRID_HEIGHT)
+# Dimensions of each block in pixels.
+BLOCK_WIDTH = 32
+BLOCK_HEIGHT = 32
 
 def main():
   window = SDL_CreateWindow(b"Tetris",
@@ -24,18 +24,47 @@ def main():
                             SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN)
   screen = SDL_GetWindowSurface(window)
 
-  dark_green = RGBToColorValue(screen, 0, 0x7f, 0)
-  SDL_FillRect(screen, None, dark_green)
+  # Game data.
+  game_grid = grid.Grid(GRID_WIDTH, GRID_HEIGHT)
 
-  SDL_UpdateWindowSurface(window)
+  GRID_PALETTE = {
+    # Grayscale colors.
+    0: RGBToColorValue(screen, 0x00, 0x00, 0x00),
+    1: RGBToColorValue(screen, 0x80, 0x80, 0x80),
+    2: RGBToColorValue(screen, 0xc0, 0xc0, 0xc0),
+    3: RGBToColorValue(screen, 0xff, 0xff, 0xff),
+
+    # Full RGB colors.
+    4: RGBToColorValue(screen, 0xff, 0x00, 0x00),
+    5: RGBToColorValue(screen, 0x00, 0xff, 0x00),
+    6: RGBToColorValue(screen, 0x00, 0x00, 0xff),
+    7: RGBToColorValue(screen, 0xff, 0xff, 0x00),
+    8: RGBToColorValue(screen, 0x00, 0xff, 0xff),
+    9: RGBToColorValue(screen, 0xff, 0x00, 0xff),
+
+    # Half RGB colors.
+    10: RGBToColorValue(screen, 0x80, 0x00, 0x00),
+    11: RGBToColorValue(screen, 0x00, 0x80, 0x00),
+    12: RGBToColorValue(screen, 0x00, 0x00, 0x80),
+    13: RGBToColorValue(screen, 0x80, 0x80, 0x00),
+    14: RGBToColorValue(screen, 0x00, 0x80, 0x80),
+    15: RGBToColorValue(screen, 0x80, 0x00, 0x80),
+  }
+  grid_renderer = grid.GridRenderer(BLOCK_WIDTH, BLOCK_HEIGHT, GRID_PALETTE)
+
+  # Draw the grid.
+  for x in xrange(15):
+    game_grid.SetValue(0, x, x)
+  grid_renderer.DrawToSurface(game_grid, screen)
 
   running = True
   event = SDL_Event()
   while running:
-      while SDL_PollEvent(ctypes.byref(event)) != 0:
-          if event.type == SDL_QUIT:
-              running = False
-              break
+    while SDL_PollEvent(ctypes.byref(event)) != 0:
+      if event.type == SDL_QUIT:
+        running = False
+        break
+    SDL_UpdateWindowSurface(window)
 
   SDL_DestroyWindow(window)
   SDL_Quit()
