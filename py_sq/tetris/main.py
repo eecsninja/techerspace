92e5235c27b173eaaa36626228d0791ddfe61868
Simon Que <sque@chromium.org>
2018-07-15 18:42:15 -0700

    Use keyboard controls to move pieces around

--- py_sq/tetris/main.py
+++ py_sq/tetris/main.py
@@ -2,6 +2,7 @@ import ctypes
 import sys
 
 from sdl2 import *
+from sdl2.ext import sprite
 
 # Local imports
 import grid
@@ -25,6 +26,8 @@ def main():
                             SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN)
   screen = SDL_GetWindowSurface(window)
 
+  sprite_factory = sprite.SpriteFactory(sprite.SOFTWARE)
+
   # Game data.
   game_grid = grid.Grid(GRID_WIDTH, GRID_HEIGHT)
 
@@ -32,8 +35,12 @@ def main():
   grid_renderer = grid.GridRenderer(BLOCK_WIDTH, BLOCK_HEIGHT, GRID_PALETTE)
 
   # For testing.
-  offset = 0
+  piece_x = 0
+  piece_y = 0
   pieces = [piece.SQUARE, piece.BAR]
+  piece_sprite = SDL_CreateRGBSurface(0, BLOCK_WIDTH * 4, BLOCK_HEIGHT * 4, 32,
+                                      0, 0, 0, 0)
+  piece_dest = SDL_Rect()
 
   running = True
   event = SDL_Event()
@@ -44,16 +51,27 @@ def main():
         running = False
         break
       if event.type == SDL_KEYDOWN:
-        offset += 1
+        if event.key.keysym.sym == SDLK_UP:
+          piece_y -= 1
+        elif event.key.keysym.sym == SDLK_DOWN:
+          piece_y += 1
+        elif event.key.keysym.sym == SDLK_LEFT:
+          piece_x -= 1
+        elif event.key.keysym.sym == SDLK_RIGHT:
+          piece_x += 1
 
     # Draw the grid.
     for x in xrange(15):
-      game_grid.SetValue(0, x, (x + offset) % len(GRID_PALETTE))
+      game_grid.SetValue(0, x, x)
     grid_renderer.DrawToSurface(game_grid, screen)
 
     # Draw pieces.
     for p in pieces:
-      grid_renderer.DrawToSurface(p.grid, screen)
+      grid_renderer.DrawToSurface(p.grid, piece_sprite)
+
+      piece_dest.x = piece_x * BLOCK_WIDTH
+      piece_dest.y = piece_y * BLOCK_HEIGHT
+      SDL_BlitSurface(piece_sprite, None, screen, piece_dest)
 
     SDL_UpdateWindowSurface(window)
 
